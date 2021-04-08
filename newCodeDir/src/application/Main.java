64f9b0c9440bb09cfbc2a0f919aa66379869e9a1
Raulcis <51254835+Raulcis@users.noreply.github.com>
2021-04-08 16:30:26 -0700

    Update Main.java

--- newCodeDir/src/application/Main.java
+++ newCodeDir/src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
-	
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import javafx.application.Application;
 import javafx.event.EventType;
@@ -15,6 +18,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -25,7 +29,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-//MOMO's code:
+
 
 public class Main extends Application {
 	private double mainRectWidth = 1100, mainRectHeight = 650;
@@ -33,17 +37,20 @@ public class Main extends Application {
     private Stage secondStage = null;     
     private Scene thirdScene = null;
     private Stage thirdStage = null;
- //   private Text loginText = null;
     private Button cancel = null;
     private Button register = null;
     private TextField userNameField = null;
     private TextField passwordField = null;
+    private TextField passwordField2 = null;
+    private int flag = 0; // flag used to see what type of error it is. 0 = incorrect username or password 
+    					  // 1 =  password entries did not match when creating account 2 = user username already exist 3 = account already exist
+   
     String file = "Accounts.txt"; // I added this file to my project,
                                   //couldn't use paths as it kept giving me errors but we should eventually change it to a path
      
 	@Override
 	public void start(Stage stage) throws IOException {
-		//File file = new File("Accounts.txt");
+
 
 		/* Making the BlueBackGroundRectangle */
 	    Rectangle rect1 = new Rectangle(mainRectWidth, mainRectHeight);
@@ -67,7 +74,6 @@ public class Main extends Application {
 	    /* Sign up Text*/
 	    Text loginText = new Text("Login");
 	    Font fontFredoka = Font.loadFont(Main.class.getResource("/application/resources/FredokaOne-Regular.ttf").toExternalForm(), 30);
-	    //my code didnt show font properly so I tried this, didn't work but I think its because my paths are messed up
 	    loginText.setFont(Font.font("/application/resources/FredokaOne-Regular.tff",30)/*fontFredoka*/);
 	    
 	    userNameField = new TextField("Enter User Name");
@@ -116,15 +122,15 @@ public class Main extends Application {
 	    loginVBox.setSpacing(10d);
 	    
 	  
-	      //Creating a Grid Pane 
-	      GridPane gridPane = new GridPane();    
+	    //Creating a Grid Pane 
+	    GridPane gridPane = new GridPane();    
 	
-	      //Setting the Grid alignment 
-	      gridPane.setAlignment(Pos.CENTER); 
+	    //Setting the Grid alignment 
+	    gridPane.setAlignment(Pos.CENTER); 
 	       
-	      //Arranging all the nodes in the grid 
-	      gridPane.add(loginVBox,0,0); 
-	      gridPane.add(Move,0,1); 
+	    //Arranging all the nodes in the grid 
+	    gridPane.add(loginVBox,0,0); 
+	    gridPane.add(Move,0,1); 
 
 	    /*put everything together*/
 	    loginInputBoxPane.getChildren().addAll(rect2,gridPane);
@@ -165,15 +171,35 @@ public class Main extends Application {
 	    
 	    Login.setOnAction(e->{
 
-            StackPane root2 = new StackPane();
-            Label label = new Label("Your are now signed in");
-            root2.getChildren().add(label);
-            Scene secondScene = new Scene(root2, 500,500);
-            Stage secondStage = new Stage();
-            secondStage.setScene(secondScene); // set the scene
-            secondStage.setTitle("Second Form");
-            secondStage.show();
-            stage.close(); // close the first stage (Window)
+            boolean succesfull=false;
+            
+            try {
+            	/*checks if login credentials match with account in file, false tells method it is not creating a new account*/
+ 			succesfull=StoreAccountInfo(MouseEvent.MOUSE_CLICKED,file,false);
+ 		} catch (IOException e1) {
+ 			e1.printStackTrace();
+ 		}
+            
+            /*if the login credentials match then open main menu*/
+            if (succesfull == true) {
+            	 StackPane root2 = new StackPane();
+                 Label label = new Label("Your are now signed in");
+                 root2.getChildren().add(label);
+                 Scene secondScene = new Scene(root2, 500,500);
+                 Stage secondStage = new Stage();
+                 secondStage.setScene(secondScene);
+                 secondStage.setTitle("Second Form");
+                 secondStage.show();
+                 stage.close(); // close the first stage (Window)	   
+            }else {
+            		/* show error if login failed*/
+            		if(flag == 0) {
+            			String error = "Error, incorrect username or password please try again.";
+            			errorMSG(error);
+            		}
+            }
+             
+            
         });
 	    
 	    	    
@@ -190,14 +216,11 @@ public class Main extends Application {
 		    loginText2.setFont(fontFredoka);
 	    	
     	    /* Sign up Text for password*/
-    	    TextField passwordField2 = new TextField("Re-Enter Password");
+    	    passwordField2 = new TextField("Re-Enter Password");
     	    passwordField2.setPrefWidth(200);
     	    passwordField2.setMaxWidth(200);
     	    
-    	    /*create the buttons for sing up page
-    	    cancel = new Button("Cancel");
-    	    register = new Button("Register");
-    	    */
+    	    /*create the buttons for sing up page*/
     	    cancel.setStyle("-fx-background-color: #AB81CD; -fx-background-radius: 15px; -fx-text-fill: #ffffff");
     	    cancel.setCursor(Cursor.HAND);
     	    
@@ -228,29 +251,188 @@ public class Main extends Application {
             boolean succesfull=false;
             	             
            try {
-			StoreAccountInfo(MouseEvent.MOUSE_CLICKED,file);
-			succesfull = true;
+        	   /* calls method that creates accounts while also making sure account is brand new and passwords entry match*/
+			succesfull=StoreAccountInfo(MouseEvent.MOUSE_CLICKED,file,true);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
            
-           if (succesfull = true) {
+           /*If account is successfully made then it opens the main menu*/
+           if (succesfull == true) {
         	   StackPane root2 = new StackPane();
                Label label = new Label("Your are now signed in");
                root2.getChildren().add(label);
                Scene secondScene = new Scene(root2, 500,500);
                Stage secondStage4 = new Stage();
-               secondStage4.setScene(secondScene); // set the scene
+               secondStage4.setScene(secondScene);
                secondStage4.setTitle("Second Form");
                secondStage4.show();
                secondStage.close(); // close the first stage (Window)   	   
+           }else {
+        	   
+        	   /*displays the error that occured when making an account*/
+               if(flag == 3) {
+            	   String error = "Error, this account already exist";
+               	errorMSG(error);
+               }else if (flag == 1) {
+   					String error = "Error,your password entries did not match. Please try again.";
+   					errorMSG(error);
+       		   }else if (flag == 2) {
+       			   String error = "Error, that user username already exist. Please sign in or use a new username.";
+       			   errorMSG(error);
+       		}
            }
-            
+           
             });
         
         });    
-	    
-        cancel.setOnAction(e->{
+	   
+	    cancelButton(registerBoxPane,registerBoxPane2,loginVBox,vbox1,hbox,rect1,fontFredoka);
+
+	}
+
+	
+	public boolean StoreAccountInfo(EventType<MouseEvent> mouseClicked, String file, boolean create) throws IOException {
+		
+		StringBuilder username = new StringBuilder();
+		StringBuilder password = new StringBuilder();
+		StringBuilder reEnterPass = new StringBuilder();
+		boolean isItDuplicate =false;
+		
+		/*gets input from textbox and stores them in username and password*/
+		username.append(userNameField.getText().toString());
+		password.append(passwordField.getText().toString());
+		
+		/*if passwordField is not null then the user must be signing up which the checks to see if passwords match*/
+		if(passwordField2 != null) {
+			
+			reEnterPass.append(passwordField2.getText().toString());
+			
+			if (!password.toString().contentEquals(reEnterPass)) {
+				flag = 1;
+				return false;
+			}
+				
+		}
+		
+		/*checks if username already exist*/
+		if(checkIfUsernameDup(file,username)) {
+			flag=2;
+			return false;
+		}
+		
+		/*checks if an account with exact same name and password exist*/
+		if(create == true) {
+		isItDuplicate = readFile(file,username,password);
+		if(isItDuplicate == true) {
+			flag = 0;
+			return false;
+		}else{
+		/*no duplicate account is detected so it beings saving the information*/	
+		FileWriter write = new FileWriter(file,true);
+		write.write(username.toString());
+		write.write(" ");
+		write.write(password.toString());
+		write.write("\n");
+		write.close();
+		return true;
+		}
+		}else{
+			/*the user was not creating a class, instead he was signing in so we check if login credentials match and exist*/
+			return readFile(file,username,password);
+		}
+	}
+	public boolean readFile(String file,StringBuilder username,StringBuilder password) 
+	{    
+		String testUser,Pass = null;
+		
+		try {
+		
+			FileReader Accountfile = new FileReader(file);
+		    BufferedReader buff = new BufferedReader(Accountfile);
+		    boolean eof = false;
+		
+		while(!eof) //does this while its not the end of file
+		{
+			
+			String line = buff.readLine(); // gets a line from the file
+
+			if(line==null)
+				eof = true; // if the line is null then its the end of the file 
+			
+			if(line != null)
+			{
+				StringTokenizer st = new StringTokenizer(line);	// converts line into tokens 
+				
+					while(st.hasMoreTokens()) // while their is tokens it will continue 
+					{
+						testUser = st.nextToken(); // gets token from converted line and puts it in testUser
+						if(st.hasMoreTokens())
+							Pass = st.nextToken();
+						
+						/* checks to see if login information matches an account in the file*/
+						if(testUser.contentEquals(username) && Pass.contentEquals(password)) {
+							flag = 3;
+							buff.close();
+							return true;
+						}
+					}			
+			}		
+}
+		buff.close();
+		}catch(IOException e){
+			
+			System.out.printf("Error -- %s ", e.toString()); // prints out exception if their is one 
+		}
+    
+		return false;
+	   }
+
+	public boolean checkIfUsernameDup(String file,StringBuilder username) 
+	{    
+		String testUser;
+		
+		try {
+		
+			FileReader Accountfile = new FileReader(file);
+		    BufferedReader buff = new BufferedReader(Accountfile);
+		    boolean eof = false;
+		
+		while(!eof) //does this while its not the end of file
+		{
+			
+			String line = buff.readLine(); // gets a line from the file
+
+			if(line==null)
+				eof = true; // if the line is null then its the end of the file 
+			
+			if(line != null)
+			{
+				StringTokenizer st = new StringTokenizer(line);	// converts line into tokens 
+				
+					while(st.hasMoreTokens()) // while their is tokens it will continue 
+					{
+						testUser = st.nextToken(); // gets token from converted line and puts it in testUser
+						
+						/*checks if username already exist*/
+						if(testUser.contentEquals(username)) {
+							buff.close();
+							return true;
+						}
+					}			
+			}		
+}
+		buff.close();
+		}catch(IOException e){
+			
+			System.out.printf("Error -- %s ", e.toString()); // prints out exception if their is one 
+		}
+    
+		return false;
+	   }
+
+    public void cancelButton(StackPane registerBoxPane,StackPane registerBoxPane2,VBox loginVBox,VBox vbox1,HBox hbox,Rectangle rect1,Font fontFredoka ) {
+            cancel.setOnAction(e->{
             
         	/*close sign up page*/
             secondStage.close();
@@ -277,25 +459,54 @@ public class Main extends Application {
             thirdStage.show();
             
         });    
-	}
+    	
+    }
+    public void errorMSG(String msg) {
+    	
+    	StackPane root10 = new StackPane();
+        Label label = new Label(msg);
+        
+        /*create background*/
+        Rectangle background = new Rectangle(mainRectWidth, mainRectHeight);
+        background.setFill(Color.web("#DEE7EC",1));
+        background.setArcHeight(40.0);
+        background.setArcWidth(40.0);
+       
+        /*create button*/
+        Button okButton = new Button("Ok");
+        
+        okButton.setStyle("-fx-background-color: #AB81CD; -fx-background-radius: 15px; -fx-text-fill: #ffffff");
+	    okButton.setCursor(Cursor.HAND);
+	    
+	    StackPane stack = new StackPane();
+	    VBox layout = new VBox();
+	    BorderPane center= new BorderPane();
+	    
+	    /*align button and label in the correct spot*/
+	    layout.getChildren().addAll(label,okButton);
+	    layout.setSpacing(50);
+	    layout.setAlignment(Pos.CENTER);
+	    
+	    /*put background,button, and label layout together*/
+	    stack.getChildren().addAll(background,layout);
+	    center.setCenter(stack);
 
-	
-	public void StoreAccountInfo(EventType<MouseEvent> mouseClicked, String file) throws IOException {
-		StringBuilder username = new StringBuilder();
-		StringBuilder password = new StringBuilder();
-		username.append(userNameField.getText().toString());
-		password.append(passwordField.getText().toString());
-		
-		FileWriter write = new FileWriter(file,true);
-		write.write(username.toString());
-		write.write(" ");
-		write.write(password.toString());
-		write.write("\n");
-		write.close();
-	}
-	
+	    /*create scene and stage*/
+        root10.getChildren().add(center);
+        Scene secondScene = new Scene(root10,600, 600);
+        Stage secondStage5 = new Stage();
+        secondStage5.setScene(secondScene); 
+        secondStage5.setTitle("Second Form");
+        secondStage5.show();
+     	 
+        /*close error pop up page*/
+        okButton.setOnAction(e3->{  
+        	
+            secondStage5.close();
+        });       	   
+    	
+    }
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
-
