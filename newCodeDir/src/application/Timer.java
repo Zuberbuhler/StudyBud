95e625ef352e54083dbbf1aa7264eaa1f7f60886
Isabella <47909035+isabellapiz@users.noreply.github.com>
2021-04-09 18:31:46 -0700

    Add files via upload

--- newCodeDir/src/application/Timer.java
+++ newCodeDir/src/application/Timer.java
@@ -1,54 +1,130 @@
 package application;
 
 public class Timer {
-	private int currentTime;
-	private int hours; 
-	private int minutes;
-	private int seconds;
-	private int elapsedTime;
-	private boolean timerRunning;
+	// Returns if the timer is running
+	private boolean running;
+	// Returns if the timer is paused
+	private boolean paused;
+	// The start time in microseconds
+	private long startTime;
+	// The time for the current paused time
+	private long pausedTime;
+	// The end time
+	private long endTime;
 	
-	public Timer() {
-		currentTime = 0; // in seconds
-		hours = 0;
-		minutes = 0;
-		seconds = 0;
-		elapsedTime = 0;
-		timerRunning = false; 
+	// Constructor for Timer
+	public Timer()
+	{
+		// Initialize time values to 0
+		pausedTime = 0;
+		startTime = 0;
+		endTime = 0;
 	}
 	
-	public int getMinutes() {
-		return minutes;
+	// Determines if the timer is running
+	public boolean isRunning() {
+		return running;
 	}
 	
-	public int getSeconds() {
-		return seconds; 
+	// Determines if the timer is paused
+	public boolean isPaused() {
+		return paused;
 	}
 	
-	public void startTimer() {
-		timerRunning = true; 
+	// Starts the timer
+	public void start() {
+		startTime = System.nanoTime();
+		running = true;
+		paused = false;
+		// Set paused time to invalid value
+		pausedTime = -1;
+	}
+	
+	// Stops the timer and returns the time elapsed since start
+	public long stop() {
+		// If timer is not running return invalid value
+		if(running == false)
+			return -1;
 		
-		// start timer and update elapsedTime and 
+		// If timer is paused then return the elapsed time
+		else if(paused)
+		{
+			running = false;
+			paused = false;
+			return pausedTime - startTime;
+		}
 		
-		hours = (elapsedTime / 3600000);
-		minutes = (elapsedTime / 60000) % 60;
-		seconds = (elapsedTime / 1000) % 60;
+		// If the timer is running and has not been paused set the end time and then return the elapsed time
+		else {
+			endTime = System.nanoTime();
+			running = false;
+			return endTime - startTime;
+		}
 	}
 	
-	public void addTime(int hours, int minutes, int seconds) {
-		currentTime += (hours*360) + (minutes*60) + seconds;
+	// Pauses the timer and returns the time elapsed so far
+	public long pause()
+	{
+		// If the timer is already stopped invalid
+		if(running == false)
+			return -1;
+		
+		// If timer is now paused return the elapsed time
+		if(paused)
+		{
+			return pausedTime - startTime;
+		}
+		
+		// Else if the timer is currently running
+		else {
+			pausedTime = System.nanoTime();
+			paused = true;
+			return(pausedTime - startTime);
+		}
 	}
 	
-	public void stopTime() {
-		timerRunning = false;
+	// Resumes the timer from paused state
+	public void resume()
+	{
+		// If paused but not stopped
+		if(paused && running)
+		{
+			startTime = System.nanoTime() - (pausedTime - startTime);
+			paused = false;
+		}
 	}
 	
-	public void restartTimer() {
-		timerRunning = false;
-		startTime = 0; 
-		elapsedTime = 0;
-		hours = 0;
-		minutes = 0;
-		seconds = 0;
+	// Returns the elapsed time
+	public long elapsed()
+	{
+		if(running) 
+		{
+			if(paused)
+				return pausedTime-startTime;
+			return(System.nanoTime() - startTime);
+		}
+		else
+			return endTime-startTime;
 	}
+	
+	// Returns the number of seconds the timer has elapsed
+	public String toString()
+	{
+		long enlapsed = elapsed();
+		return((double) enlapsed / 1000000000.0) + " Seconds";
+	}
+	
+	public String convertNanoseconds()
+	{
+		long nanoSeconds = elapsed();
+		long seconds = (long) (nanoSeconds / 1e9);
+		int minutes = (int) Math.floor((seconds / 60));
+		
+		String time = minutes+":"+seconds;
+		return time;
+		
+	}
+	
+	
+	
 }
