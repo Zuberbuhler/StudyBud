c1a996b76727fa17fd6edcc09b40dbf0919e9d5f
tiffanyadrienne <77076527+tiffanyadrienne@users.noreply.github.com>
2021-04-23 16:51:30 -0700

    Add files via upload

---
+++ newCodeDir/4.23.2021/src/application/HomePageToolBarGUI.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class HomePageToolBarGUI {
+//not sure if this should be a class of its own since it's really simple
+	
+	  public HBox toolBar() {
+			// tool bar: message 
+		    Text toolBarMessage = new Text("Welcome!");
+		    toolBarMessage.setFont(Font.font("Arial",FontWeight.EXTRA_BOLD, 30));
+		    
+		    // tool bar: logo image 
+		    Image img = new Image(getClass().getResourceAsStream("/application/resources/logoTransparentSmall.png"));
+		    ImageView imgView = new ImageView(img);
+		    imgView.setFitWidth(100);
+		    imgView.setFitHeight(100);
+		    
+		    // tool bar: logout button
+		    Button logoutBtn = new Button("Logout");
+		    logoutBtn.setFont(Font.font("Arial",FontWeight.EXTRA_BOLD, 20));
+		    logoutBtn.setStyle("-fx-background-color: thistle;");
+		    //logoutBtn.setPrefSize(, 10);
+
+		    // horizontal layout for tool bar 
+		    HBox toolBarContent = new HBox();
+		    toolBarContent.getChildren().addAll(imgView, toolBarMessage, logoutBtn);
+		    toolBarContent.setAlignment(Pos.CENTER);
+		    toolBarContent.setSpacing(100);
+		    toolBarContent.setStyle("-fx-background-color: lightblue;"); // change to og color !!
+		    toolBarContent.setPadding(new Insets(20,20,20,20));
+		    
+		    return toolBarContent;
+		}
+}
---
+++ newCodeDir/4.23.2021/src/application/TaskGUI.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableView.TableViewSelectionModel;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class TaskGUI {
+	
+}
